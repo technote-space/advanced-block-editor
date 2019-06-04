bfee2fbb63d76a454fa23e9935cfc4a212d5ec67
Technote <technote.space@gmail.com>
2019-06-05 03:10:33 +0900

    update packages

--- assets/js/test/setup.js
+++ assets/js/test/setup.js
@@ -1,22 +1,30 @@
+const Mousetrap = require( 'mousetrap' );
+global.Mousetrap = Mousetrap;
+global.window.matchMedia = () => ( {
+	matches: true, addListener: () => {
+	},
+} );
 global.abeParams = {
 	translate: {
 		test: 'テスト',
 	},
 };
-global.wp = {};
-global.wp.components = {};
-global.wp.editor = {};
-global.wp.compose = {
-	compose: () => value => value,
-	createHigherOrderComponent: value => value,
-};
-global.wp.data = {
-	withSelect: () => {
-	},
-	withDispatch: () => {
-	},
-	registerStore: () => {
-	},
-};
-global.wp.element = {};
 
+const blockLibrary = require( '@wordpress/block-library' );
+const blocks = require( '@wordpress/blocks' );
+const components = require( '@wordpress/components' );
+const editor = require( '@wordpress/editor' );
+const compose = require( '@wordpress/compose' );
+const data = require( '@wordpress/data' );
+const element = require( '@wordpress/element' );
+global.wp = {
+	blockLibrary,
+	blocks,
+	components,
+	editor,
+	compose,
+	data,
+	element,
+};
+
+blockLibrary.registerCoreBlocks();
