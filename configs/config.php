dc8561cc84f8ebb44af3cce215b54cf08609eadb
Technote <technote.space@gmail.com>
2019-07-17 14:14:40 +0900

    feat: add auto update

--- configs/config.php
+++ configs/config.php
@@ -15,6 +15,9 @@ return [
 	// required wordpress version
 	'required_wordpress_version' => '5.0',
 
+	// update
+	'update_info_file_url'       => 'https://raw.githubusercontent.com/technote-space/advanced-block-editor/master/update.json',
+
 	// github repo
 	'github_repo'                => 'technote-space/advanced-block-editor',
 ];
