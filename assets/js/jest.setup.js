a1970bd2f3210d84bc789742740c6dd28060c96a
Technote <technote.space@gmail.com>
2020-02-19 01:08:33 +0900

    feat: use test library

--- assets/js/jest.setup.js
+++ assets/js/jest.setup.js
@@ -1,57 +1,16 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { setupGlobal } from '@technote-space/gutenberg-test-helper';
 
-Enzyme.configure( {
-	adapter: new Adapter(),
-	snapshotSerializers: [ 'enzyme-to-json/serializer' ],
-} );
-
-const Mousetrap = require( 'mousetrap' );
-const lodash = require( 'lodash' );
-global.Mousetrap = Mousetrap;
-global.window.lodash = lodash;
-global.window.matchMedia = () => ( {
-	matches: true, addListener: () => {
+setupGlobal( {
+	setUseRefMock: false,
+	mockLodashDebounce: false,
+	globalParams: {
+		abeParams: {
+			translate: {
+				test: 'テスト',
+			},
+		},
+	},
+	setWp: {
+		formatLibrary: false,
 	},
 } );
-global.abeParams = {
-	translate: {
-		test: 'テスト',
-	},
-};
-
-const blockLibrary = require( '@wordpress/block-library' );
-const blockEditor = require( '@wordpress/block-editor' );
-const blocks = require( '@wordpress/blocks' );
-const components = require( '@wordpress/components' );
-const compose = require( '@wordpress/compose' );
-const coreData = require( '@wordpress/core-data' );
-const data = require( '@wordpress/data' );
-const editor = require( '@wordpress/editor' );
-const element = require( '@wordpress/element' );
-const hooks = require( '@wordpress/hooks' );
-const i18n = require( '@wordpress/i18n' );
-const keycodes = require( '@wordpress/keycodes' );
-const richText = require( '@wordpress/rich-text' );
-const url = require( '@wordpress/url' );
-
-global.wp = {
-	blockLibrary,
-	blockEditor,
-	blocks,
-	components,
-	compose,
-	coreData,
-	data,
-	editor,
-	element,
-	hooks,
-	i18n,
-	keycodes,
-	richText,
-	url,
-};
-
-blockLibrary.registerCoreBlocks();
-
-hooks.removeAllFilters( 'editor.BlockEdit' );
