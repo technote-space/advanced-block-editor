3189927b7d39d25c9fb9e33340b49f22673872b5
Technote <technote.space@gmail.com>
2020-12-20 02:28:09 +0900

    chore: update wp version

--- autoload.php
+++ autoload.php
@@ -4,7 +4,7 @@
  * Plugin URI:
  * Description: This plugin adds some useful features to the block editor.
  * Author: Technote
- * Version: 1.0.11
+ * Version: 1.0.12
  * Author URI: https://technote.space
  * Text Domain: advanced-block-editor
  * Domain Path: /languages/
